404705168e861fd218c9c132db64ef8f1c7433cd
zhulei <642176940@qq.com>
2017-05-31 12:29:07 +0800

    .. & ..

--- page/component/pages/canvas/canvas.js
+++ page/component/pages/canvas/canvas.js
@@ -1,26 +1,36 @@
-let _width;
-let ctx;
-let center;
-let _angle = 0;
-let curColor;
-let speed = 16;
-const strokeWidth = 16;
-const colors = ['#88ff00', '#ff0088', '#0088ff', '#ff8800'];
+let _width; // 屏幕宽度
+let ctx;    // 画布操作对象
+let center; // 画布中心坐标
+let _angle = 0; // 当前旋转角度
+let curColor;   // 当前指针颜色
+let speed = 7;  // 当前旋转速度
+let catchMatchColor; // 是否匹配对应颜色标记
+const strokeWidth = 12; // 绘图宽度
+// const colors = ['red', 'blue', 'yellow', 'Moccasin', 'Aquamarine', 'BlueViolet', 'DarkSeaGreen']
+const colors = ['red', 'blue', 'yellow', 'Moccasin'];
 
 function draw(colors, _color, step) {
   if (!ctx) return;
-
-  _angle = (_angle + (step * speed))
-  const needleLength = _width / 3.5
-  const arcAngle = _angle / 360 * Math.PI
-  const radius = center * 0.75;
-  const spaceAngle = 10 / 360;
-  const stepAngle = (2 * Math.PI) / colors.length;
-  let startAngle = 0 - (stepAngle / 2);
+  const colorNum = colors.length;
+  const arcAngle = _angle / 360 * Math.PI            // 旋转弧度
+  const needleLength = _width / 3.5                  // 指针长度
+  const radius = center * 0.75;                      // 外圈半径
+  const spaceAngle = 10 / 360;                       // 外圈间隔
+  const stepAngle = (2 * Math.PI) / colorNum;   // 外圈分布弧长
+  let startAngle = 0 - (stepAngle / 2);              // 起点x轴对称
   let index = 0;
 
-  ctx.setLineWidth(strokeWidth)
-  while (index < colors.length) {
+  // 是否开始拦截
+  let matchIndex = Math.floor((arcAngle + stepAngle / 2) / stepAngle) % colorNum
+  matchIndex = (matchIndex + colorNum) % colorNum;
+  if (catchMatchColor && colors[matchIndex] != curColor) {
+    console.error('!!!!!');
+    speed = 0;
+  }
+  catchMatchColor = colors[matchIndex] == curColor
+
+  // 画外圈
+  while (index < colorNum) {
     ctx.beginPath()
     ctx.setStrokeStyle(colors[index])
     ctx.arc(center, center, radius, startAngle + spaceAngle, startAngle + stepAngle - spaceAngle)
@@ -29,13 +39,26 @@ function draw(colors, _color, step) {
     ctx.stroke();
   }
 
+  // 内部指针
   ctx.beginPath()
   ctx.setStrokeStyle(_color)
   ctx.moveTo(center, center)
-  ctx.lineTo(center + Math.sin(arcAngle) * needleLength, center - Math.cos(arcAngle) * needleLength)
+  ctx.lineTo(center + Math.cos(arcAngle) * needleLength, center + Math.sin(arcAngle) * needleLength)
   ctx.stroke()
+  ctx.draw()
+
+  _angle = (_angle + (step * speed))                 // 更新旋转角度
 }
 
+function getNewColor(curColor) {
+  const newColor = colors[Math.floor(Math.random() * colors.length)];
+  if (curColor == newColor) {
+    return getNewColor(curColor);
+  }
+  return newColor;
+}
+
+// 获取屏幕宽度
 wx.getSystemInfo({
   success: function(res) {
     _width = res.screenWidth;
@@ -43,27 +66,29 @@ wx.getSystemInfo({
   },
 })
 
+// 定义页面内容
 Page({
   data: {
     rStep: 1
   },
   start: function (e) {
-    curColor = colors[Math.floor(Math.random() * colors.length)]
-    speed = (speed - 1) % 17 || 16
+    curColor = getNewColor(curColor);
+    catchMatchColor = false;
+    // speed = (speed - 1) % 17 || 16
     console.log(speed);
     this.setData({
-      rStep: this.data.rStep * -1
+      rStep: this.data.rStep * -1 // 改变旋转方向
     })
   },
   move: function (e) { },
   end: function (e) { },
   onLoad: function () {
-    ctx = wx.createCanvasContext('myCanvas')
-    curColor = colors[0]
     const _this = this;
+    curColor = getNewColor(colors[0])
+    ctx = wx.createCanvasContext('myCanvas')
+    ctx.setLineWidth(strokeWidth)
     setInterval(function () {
       draw(colors, curColor, _this.data.rStep)
-      ctx.draw()
     }, 17);
   }
 })
